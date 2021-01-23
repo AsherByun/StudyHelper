f8dfcdd5c38b47257b44e078b5cb06c86663627e
변성재 <dfdo333@naver.com>
2021-01-24 00:26:34 +0900

    password encoder & insert test

--- src/main/java/com/studyhelper/security/MemberDetailService.java
+++ src/main/java/com/studyhelper/security/MemberDetailService.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.studyhelper.member.dao.MemberRepository;
 import com.studyhelper.member.dto.Member;
 
+@Service
 public class MemberDetailService implements UserDetailsService {
 	private final MemberRepository memberRepo;
 
---
+++ src/test/java/com/studyhelper/member/MemberTest.java
@@ -0,0 +1,36 @@
+package com.studyhelper.member;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.studyhelper.member.dao.MemberRepository;
+import com.studyhelper.member.dto.Member;
+import com.studyhelper.properties.Gender;
+import com.studyhelper.properties.Role;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Commit
+public class MemberTest {
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Test
+	public void 맴버삽입테스트() {
+		Member member = new Member();
+		member.setAge(27);
+		member.setId("dfdo333");
+		member.setGender(Gender.MAN);
+		member.setPassword(encoder.encode("1234"));
+		member.setNickName("hohoho");
+		member.setRole(Role.ROLE_ADMIN);
+		memberRepository.save(member);
+	}
+}
